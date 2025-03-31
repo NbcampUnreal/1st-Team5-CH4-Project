d1b2be73c0b1f52e672c5c66a82a1b50501f940f
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-03-31 19:24:04 +0900

    개인전 게임 모드 추가
    [🎉 add ] GF_FFAGameMode 추가
    [✨ update ] GladiatorsFFantasy.Build.cs 파일 수정
    -> 경로 찾는 코드 추가했습니다.

---
+++ Source/GladiatorsFantasy/Server/GF_FFAGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GF_FFAGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORSFANTASY_API AGF_FFAGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
