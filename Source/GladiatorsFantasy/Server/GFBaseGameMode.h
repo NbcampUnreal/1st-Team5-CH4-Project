e82fe251add8ebcf4873b2c8e247fc3c002e6a10
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-04-02 20:50:48 +0900

    개인전 난투전 로직 구현 (위젯 추가)
    [ 🎉 add ] FFAScore, MatchResult Widget
    [ 🎉 add ] Widget cpp file
    > 개인전 전용 위젯 추가했습니다
    [ 🎉 add ] BaseGameMode
    > 최상위 부모 게임 모드 추가했습니다

---
+++ Source/GladiatorsFantasy/Server/GFBaseGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GFBaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORSFANTASY_API AGFBaseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
