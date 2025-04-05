91ec39234f66d281b968b423d2502a709c5890d6
swehio <63448248+swehio@users.noreply.github.com>
2025-04-06 00:09:13 +0900

    스킬 쿨타임 UI에 무기 등급별 스킬 활성화 반영
    [✨Update] WBP_GFSkillCooldownWidget
    등급별로 사용가능한 스킬 활성화 로직 구현
    
    [✨Update] CombatStateManager
    SkillHUD 위젯 생성 로직 추가
    
    [✨Update] MainCharacter
    SkillHUD 위젯 생성 로직 CombatStateManager로 이동 및 기타 수정
    
    [🎉Add]  GFSkillCooldownWidgetSlot
    스킬 UI 스킬별 Slot 분리
    
    [❗Caution] WBP_GFSkillCooldownWidget
    다른 분들의 UI와 통합 논의 필요

---
+++ Source/GladiatorsFantasy/Skill/GFSkillCooldownWidgetSlot.h
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GFSkillCooldownWidgetSlot.generated.h"
+
+class UCanvasPanel;
+class UHorizontalBox;
+class UOverlay;
+class UProgressBar;
+class UTextBlock;
+
+USTRUCT(BlueprintType)
+struct FSkillCooldownData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UProgressBar> CooldownProgressBar;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UTextBlock> CooldownTextBlock;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CooldownTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTimerHandle CooldownTimerHandle;
+
+	FSkillCooldownData()
+		: CooldownProgressBar(nullptr)
+		, CooldownTextBlock(nullptr)
+		, CooldownTime(0.0f)
+	{
+	}
+};
+
+UENUM(BlueprintType)
+enum class ESkillRarity_Type : uint8
+{
+	NomalSkill, RareSkill, EpicSkill, LegendarySkill
+};
+
+
+UCLASS()
+class GLADIATORSFANTASY_API UGFSkillCooldownWidgetSlot : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+	
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSkillCooldownData CooldownData;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UProgressBar> SkillProgressBar;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> SkillTextBlock;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CooldownTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTimerHandle CooldownTimerHandle;
+
+	bool IsSkillCooldownActive;
+
+	UFUNCTION(BlueprinTCallable)
+	void UpdateSkillUI();
+
+	UFUNCTION(BlueprinTCallable)
+	void SetSkillCooldownActive(bool bIsActive);
+
+	UFUNCTION(BlueprintCallable)
+	void SetCooldownDataMap(FTimerHandle SkillTimer, float SkillCooldownTime);
+
+};
